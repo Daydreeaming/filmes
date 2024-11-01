4ffce585b1afab243bf7241ba7b8fab2f77bc2e2
Daydreeaming <lucascamposdantas@hotmail.com>
2024-11-01 01:08:22 -0300

    feat: pagina de login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,31 @@
 <template>
-	<div>Teste</div>
+	<v-app>
+		<v-container class="d-flex align-center justify-center" style="min-height: 100vh;">
+			<v-card min-width="400" class="pa-5">
+				<v-card-title class="text-h5">Login</v-card-title>
+				<v-card-text>
+					<v-form>
+						<v-card-text class="pl-0">
+							<v-text-field label="E-mail" variant="outlined" required></v-text-field>
+						</v-card-text>
+						<v-card-text class="pl-0">
+							<v-text-field type="password" label="Senha" variant="outlined" required></v-text-field>
+						</v-card-text>
+						<v-container class="d-flex justify-space-between pl-0 pt-0">
+							<v-btn color="red" class="mt-4">Esqueceu a senha</v-btn>
+							<v-btn color="red" class="mt-4" @click="testea">Enviar</v-btn>
+						</v-container>
+					</v-form>
+				</v-card-text>
+			</v-card>
+		</v-container>
+	</v-app>
 </template>
 
+<style></style>
+
 <script setup lang="ts">
-//
+function testea() {
+	console.log('Ui');
+}
 </script>
--- src/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>testeadssad</div>
-</template>
-
-<script lang="ts" setup>
-//
-</script>
