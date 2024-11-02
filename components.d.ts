d890398e999895408a6a06fd3723094fbf616714
Daydreeaming <lucascamposdantas@hotmail.com>
2024-11-02 00:34:15 -0300

    feat: adiciona rotas novas na tela de login e validacoes dos inputs

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    copy: typeof import('./src/components/HelloWorld copy.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,16 +4,22 @@
 			<v-card min-width="400" class="pa-5">
 				<v-card-title class="text-h5">Login</v-card-title>
 				<v-card-text>
-					<v-form>
+					<v-form ref="form">
 						<v-card-text class="pl-0">
-							<v-text-field label="E-mail" variant="outlined" required></v-text-field>
+							<v-text-field label="E-mail" v-model="email" :rules="emailRules" variant="outlined"
+								required></v-text-field>
 						</v-card-text>
 						<v-card-text class="pl-0">
-							<v-text-field type="password" label="Senha" variant="outlined" required></v-text-field>
+							<v-text-field type="password" v-model="senha" :rules="passwordRules" label="Senha"
+								variant="outlined" required></v-text-field>
 						</v-card-text>
 						<v-container class="d-flex justify-space-between pl-0 pt-0">
-							<v-btn color="red" class="mt-4">Esqueceu a senha</v-btn>
-							<v-btn color="red" class="mt-4" @click="testea">Enviar</v-btn>
+							<router-link to="/forget-password">
+								<v-btn color="red" class="mt-4">Esqueceu a senha</v-btn>
+							</router-link>
+							<router-link to="/home">
+								<v-btn color="red" :disabled="!isValid" class="mt-4">Entrar</v-btn>
+							</router-link>
 						</v-container>
 					</v-form>
 				</v-card-text>
@@ -25,7 +31,27 @@
 <style></style>
 
 <script setup lang="ts">
-function testea() {
-	console.log('Ui');
-}
+import { ref, watch } from 'vue'
+
+const form = ref();
+const email = ref('');
+const senha = ref('');
+const isValid = ref(false);
+
+const emailRules = [
+	(field: string) => !!field || 'O e-mail é um campo obrigatório',
+	(field: string) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(field) || 'E-mail tem que ser um campo válido.'
+];
+
+const passwordRules = [
+	(field: string) => !!field || 'A senha é um campo obrigatório',
+];
+
+watch([email, senha], async () => {
+	const emailValid = emailRules.every(rule => rule(email.value) === true);
+	const passwordValid = passwordRules.every(rule => rule(senha.value) === true);
+
+	isValid.value = emailValid && passwordValid;
+});
+
 </script>
